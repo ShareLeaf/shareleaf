f15f7b20def4a17476b5eff22e97d1a6274a5e05
Biz Melesse <bizmelesse@gmail.com>
2022-06-22 23:36:18 -0700

    SL-8 Set basic request headers

--- backend/shareleaf-api/src/main/java/co/shareleaf/service/scraper/ScraperUtils.java
+++ backend/shareleaf-api/src/main/java/co/shareleaf/service/scraper/ScraperUtils.java
@@ -19,6 +19,10 @@ public record ScraperUtils(ErrorListener errorListener) {
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
         webClient.getOptions().setTimeout(5000);
+        webClient.addRequestHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36");
+        webClient.addRequestHeader("pragma", "no-cache");
+        webClient.addRequestHeader("cache-control", "no-cache");
+        webClient.addRequestHeader("sec-ch-ua-platform", "macOS");
         webClient.setJavaScriptErrorListener(errorListener.javaScriptErrorListener());
         return webClient;
     }
