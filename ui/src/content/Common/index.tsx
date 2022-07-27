4e20463bdc153b3ad804acdac7e9751fea4ff3d8
Biz Melesse <melesse@kippo.com>
2022-07-27 00:13:39 -0700

    SL-14 Enable blog and pwa buttons

--- ui/src/content/Common/index.tsx
+++ ui/src/content/Common/index.tsx
@@ -1,5 +1,5 @@
 import React, {FC, ReactNode} from "react";
-import {Box, Container} from "@mui/material";
+import {Box, Button, Container, Grid} from "@mui/material";
 import Head from "next/head";
 import FullLogo from "@/components/Logo/FullLogo";
 import Footer from "@/components/Footer";
@@ -19,17 +19,29 @@ const Common: FC<CommonProps> = (props) => {
             </Head>
             <Container sx={{height: '100%', padding: 0}}>
             <Container maxWidth="lg">
-                <Box display="flex" alignItems="center" sx={{marginTop: '10px'}}>
-                    <FullLogo />
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        flex={1}
-                    >
-                        <Box />
-                    </Box>
-                </Box>
+                <Grid container>
+                    <Grid item md={11} xs={10}>
+                        <Box display="flex" alignItems="center" sx={{marginTop: '10px'}}>
+                            <FullLogo />
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="space-between"
+                                flex={1}
+                            >
+                                <Box />
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item md={1} xs={2}>
+                        <Box display="flex" alignItems="right" sx={{marginTop: '10px'}}>
+                            <Button href="https://blog.shareleaf.co" variant="contained" color="primary">
+                                {'Blog'}
+                            </Button>
+                        </Box>
+                    </Grid>
+
+                </Grid>
             </Container>
             {props.children}
             {props.showHighlights && <Highlights />}
