aac65bc79d4c14bf580ac1ae20055fe1ab55b902
Biz Melesse <melesse@kippo.com>
2022-09-26 16:20:59 +0530

    SL-0 Update setup script

--- dev-ops/setup.sh
+++ dev-ops/setup.sh
@@ -33,3 +33,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+# Configure the CLI
+aws configure
+# Provide AWS Access Key ID
+# Provide AWS Secret Access Key
+
