3088e62f12a811cde2c97895dcfaef0798f5312d
Biz Melesse <melesse@kippo.com>
2023-01-04 14:01:04 -0600

    SL-1 Add REST error handling controller advice

---
+++ backend/shareleaf-api/src/main/java/co/shareleaf/utils/exception/ShareLeafValidationException.java
@@ -0,0 +1,12 @@
+package co.shareleaf.utils.exception;
+
+/**
+ * @author Biz Melesse
+ * created on 01/04/2023
+ */
+public class ShareLeafValidationException extends RuntimeException {
+
+    public ShareLeafValidationException(String message) {
+        super(message);
+    }
+}
